71db460ce135df396b80b3869ec6c1dd1a18fd30
NileshSaha <03.nileshsaha@gmail.com>
2023-01-28 00:01:53 +0530

    logout added

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -27,6 +27,10 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user)
 })
 
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -53,6 +57,9 @@ export const authSlice = createSlice({
         state.isError = true
         state.user = null
         state.message = action.payload
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
       })
       
   }
