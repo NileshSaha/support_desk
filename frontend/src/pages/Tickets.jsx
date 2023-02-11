ba43e2476a76e38591d0bdbf5a6b531e4fae2cf6
NileshSaha <03.nileshsaha@gmail.com>
2023-02-11 11:07:02 +0530

    tickets list page updated

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -4,6 +4,7 @@ import {toast} from 'react-toastify'
 import {getTickets, reset} from '../features/ticket/ticketSlice'
 import Spinner from '../components/Spinner'
 import { BackButon } from '../components/BackButon'
+import TicketItem from '../components/TicketItem'
 
 
 function Tickets() {
@@ -26,6 +27,17 @@ function Tickets() {
   return (
     <div>
       <BackButon url='/'/>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket}/>
+        ))}
+      </div>
     </div>
   )
 }
